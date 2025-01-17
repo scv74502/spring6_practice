82219f3a198968ebf20da39cd06fccc746f186d6
kunwoo park <scv74502@gmail.com>
2025-01-17 12:27:31 +0900

    inheritance, abstract class PaymentService

--- src/main/java/com/example/tobyspring/PaymentService.java
+++ src/main/java/com/example/tobyspring/PaymentService.java
@@ -12,9 +12,9 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
-public class PaymentService {
+abstract public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate();
+        BigDecimal exRate = getExRate(currency);
 
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
@@ -23,23 +23,5 @@ public class PaymentService {
                 validUntil);
     }
 
-    private BigDecimal getExRate() throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = data.rates().get("KRW");
-        System.out.println(exRate);
-        return exRate;
-    }
-
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
-    }
+    abstract BigDecimal getExRate(String currency) throws IOException;
 }
