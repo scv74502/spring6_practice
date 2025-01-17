c996672627e48dce0c1a8587ca25f5cc6a97433d
kunwoo park <scv74502@gmail.com>
2025-01-17 11:35:06 +0900

    SoC, getExRate method extract

--- src/main/java/com/example/tobyspring/PaymentService.java
+++ src/main/java/com/example/tobyspring/PaymentService.java
@@ -14,7 +14,16 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
+        BigDecimal exRate = getExRate();
+
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount,
+                validUntil);
+    }
+
+    private BigDecimal getExRate() throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -25,15 +34,7 @@ public class PaymentService {
         ExRateData data = mapper.readValue(response, ExRateData.class);
         BigDecimal exRate = data.rates().get("KRW");
         System.out.println(exRate);
-
-        // 금액 계산
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount,
-                validUntil);
+        return exRate;
     }
 
     public static void main(String[] args) throws IOException {
