3a95e664555d55805e75b63f7cf56ff6114b0762
kunwoo park <scv74502@gmail.com>
2025-01-20 11:28:44 +0900

    class seperate

--- src/main/java/com/example/tobyspring/PaymentService.java
+++ src/main/java/com/example/tobyspring/PaymentService.java
@@ -1,27 +1,23 @@
 package com.example.tobyspring;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-abstract public class PaymentService {
+public class PaymentService {
+    private SimpleExRateProvider exRateProvider;
+
+    public PaymentService() {
+        this.exRateProvider = new SimpleExRateProvider();
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
-
+        exRateProvider = new SimpleExRateProvider();
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount,
                 validUntil);
     }
-
-    abstract BigDecimal getExRate(String currency) throws IOException;
 }
