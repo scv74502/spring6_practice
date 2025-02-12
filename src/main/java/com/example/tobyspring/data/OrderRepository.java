57faf91e2fa529828a4fce017affcc323123cfe5
kunwoo park <scv74502@gmail.com>
2025-02-12 20:53:31 +0900

    DataAccessException exception abstract

--- src/main/java/com/example/tobyspring/data/OrderRepository.java
+++ src/main/java/com/example/tobyspring/data/OrderRepository.java
@@ -5,41 +5,17 @@ import com.example.tobyspring.order.Order;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.math.BigDecimal;
 
 public class OrderRepository {
-    private final EntityManagerFactory emf;
-
-    public OrderRepository(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public void save(Order order){
-        // em
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-
-        try {
-            // em.persist
-            em.persist(order);
-            em.flush();
-
-            // transaction
-            transaction.commit();
-
-        } catch (RuntimeException e) {
-            if(transaction.isActive()) {
-                transaction.rollback();
-            }
-            throw e;
-        } finally {
-            if(em.isOpen()) {
-                em.close();
-            }
-        }
+        entityManager.persist(order);
     }
 }
