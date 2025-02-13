2d2a514e96da8477ebe5dff8cf390162637d4828
kunwoo park <scv74502@gmail.com>
2025-02-13 15:57:16 +0900

    remove jpa dependency,add orm.xml

--- src/main/java/com/example/tobyspring/order/Order.java
+++ src/main/java/com/example/tobyspring/order/Order.java
@@ -4,14 +4,14 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
+//@Entity
+//@Table(name = "orders")
 public class Order {
-    @Id
-    @GeneratedValue
+//    @Id
+//    @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
+//    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
