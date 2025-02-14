16853f7a7af995985fb1f90e857707880d2dc96b
kunwoo park <scv74502@gmail.com>
2025-02-14 13:07:04 +0900

    transaction abstract, PlatformTransactionManager

--- src/main/java/com/example/tobyspring/order/OrderService.java
+++ src/main/java/com/example/tobyspring/order/OrderService.java
@@ -1,8 +1,8 @@
 package com.example.tobyspring.order;
 
-import com.example.tobyspring.data.JpaOrderRepository;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
@@ -10,9 +10,9 @@ import java.math.BigDecimal;
 @Service
 public class OrderService {
     private final OrderRepository OrderRepository;
-    private final JpaTransactionManager transactionManager;
+    private final PlatformTransactionManager transactionManager;
 
-    public OrderService(OrderRepository orderRepository, JpaTransactionManager transactionManager) {
+    public OrderService(OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
         this.OrderRepository = orderRepository;
         this.transactionManager = transactionManager;
     }
